30c9daf9b908e9051c415638e54079f1e80fbd37
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-03-24 19:40:16 -0500

    extension working

--- background.js
+++ background.js
@@ -4,29 +4,41 @@ const reqFilter = {
 
 function listener(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
+  const decoder = new TextDecoder("utf-8")
+  const encoder = new TextEncoder()
 
-  filter.onstart = (event) => {
-    console.log("start called");
-    console.log(event.data);
-  };
-
-  const data = [];
+  // filter.onstart = (event) => {
+  //   console.log("started")
+  // }
+  let str = ""
   filter.ondata = (event) => {
-    console.log("data called");
-    data.push(event.data);
-    filter.disconnect();
+    str += decoder.decode(event.data, {stream: true})
   };
+
+  filter.onstop = (event) => {
+    console.log("closing connection")
+    const data = JSON.parse(str)
+    for(const question in data.sessions) {
+      const path = data.sessions[question].question.question_info.question_responses
+      for(questionData in path){
+        if(path[questionData].correct){
+          data.sessions[question].question.question_info.question_responses[questionData].text = path[questionData].text.slice(0, -4) + " ✅</p>"
+        }
+      }
+    }
+    str = JSON.stringify(data)
+    filter.write(encoder.encode(str))
+    filter.disconnect()
+  }
 
   filter.onerror = (event) => {
     console.log(filter.error);
   };
 
-  console.log("at the end");
+
   return {};
 }
 
-browser.webRequest.onBeforeSendHeaders.addListener(listener, reqFilter, [
-  "requestHeaders",
-]);
+browser.webRequest.onBeforeRequest.addListener(listener, reqFilter, ['blocking']);
 
 console.log("extension started");
