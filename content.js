a1b22d6147450ff6b752235a2f855264952aad58
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-03-29 12:38:02 -0500

    Added switch and fixed emoji positioning

--- content.js
+++ content.js
@@ -1,24 +1,33 @@
 const button = document.createElement('button');
-button.innerText = 'Reveal';
-button.style.position = 'fixed';
-button.style.top = '0';
-button.style.left = '0';
+button.innerText = 'O';
+button.className = "reveal-btn switch-btn"
 document.body.appendChild(button);
+let isRevealed = false;
 
 button.addEventListener("click", (event) => {
-  console.log("clicked")
-  browser.runtime.sendMessage({message: "answer_request"})
-    .then(response => handleData(response.response))
+  if(isRevealed){
+    isRevealed = !isRevealed
+    button.innerText = "O"
+    button.className = "reveal-btn switch-btn"
+    document.querySelectorAll(".correct-answer").forEach(element => {
+      element.classList.remove("correct-answer")
+    })
+  }else{
+    isRevealed = !isRevealed
+    button.innerText = "X"
+    button.className = "hide-btn switch-btn"
+    browser.runtime.sendMessage({message: "answer_request"})
+      .then(response => handleData(response.response))
+  }
 })
 
 
 function handleData(answerData) {
   const elements = document.querySelectorAll("article.participant-prompt")
-  const toAdd = " ✅"
   elements.forEach((element, index) => {
     const options = element.childNodes[1].childNodes[0].childNodes[0].children
     answerData[index].forEach((answer) => {
-      options[answer].children[1].innerText += toAdd
+      options[answer].children[1].className += " correct-answer"
     })
   })
 }
